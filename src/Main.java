1bb917d5212f5f54bd5bd744109ba5faa89f5c58
aromaric <aromaricadjei213@gmail.com>
2024-12-03 23:23:00 +0000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String texte="Une fonction en Java à été exécutée";
+        affiche(texte);
+    }
+
+    public static void affiche(String texte){
+        System.out.println(texte);
+    }
+}
