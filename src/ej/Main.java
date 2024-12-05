cfd02c952723740058f054d61b223a209908b265
aromaric <aromaricadjei213@gmail.com>
2024-12-05 13:15:59 +0000

    gestion des flux en Java suite

--- src/ej/Main.java
+++ src/ej/Main.java
@@ -6,6 +6,9 @@ import ej.kits.KitDemarrage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
@@ -17,34 +20,54 @@ public class Main {
 
     public static void main(String[] args) {
 
-        logger.info("Lancement du programme Epicrafter's Journey. ");
+        logger.info("Lancement du programme Epicrafter's Journey.");
 
         try {
             // Le programme commence avec un Kit de démarrage.
             KitDemarrage kit = new KitDemarrage(constructionSetBlocs());
+
+            //kit.sauvegarder();
+
             System.out.println("Vous possédez un kit de démarrage !");
 
-            // Il affiche les mots clés associés au Kit pour donner des idées à l'utilisateur.
-            System.out.println("Voici quelques idées de constructions avec le Kit de démarrage : ");
-            Set<String> motsCles = kit.getMotsCles();
-            for(String mot : motsCles) {
-                System.out.println(mot);
+            System.out.println(
+                    "Que souhaitez-vous afficher ?"
+                            + "\n\t1 - Les idées de constructions. "
+                            + "\n\t2 - Le nombre de blocs pour chaque type de blocs présent dans le kit");
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                String reponse = reader.readLine();
+
+
+
+            if (reponse.equals("1")){
+                // Il affiche les mots clés associés au Kit pour donner des idées à l'utilisateur.
+                System.out.println("Voici quelques idées de constructions avec le Kit de démarrage : ");
+                Set<String> motsCles = kit.getMotsCles();
+                for(String mot : motsCles) {
+                    System.out.println(mot);
+                }
+            } else if (reponse.equals("2")) {
+                // Il affiche à l'utilisateur le nombre de blocs en fonction du type à contenu par le Kit.
+                System.out.println("Voici le nombre de blocs de chaque type contenu dans le Kit de démarrage : ");
+                Map<Type, Integer> quantiteBloc = new TreeMap<Type, Integer>(); // La TreeMap permet de trier les entrées par ordre alphabétique de la clé.
+                for (IBloc bloc : kit.getBlocs()) {
+                    Type type = Type.valueOf(bloc.getClass().getSimpleName().toUpperCase());
+                    int quantite = quantiteBloc.getOrDefault(type, 0) + 1; // Quantite existante + 1.
+                    quantiteBloc.put(type, quantite);
+                }
+                Set<Type> types = quantiteBloc.keySet();
+                for(Type type : types) {
+                    System.out.println(type.toString() + " " + quantiteBloc.get(type));
+                }
+            } else {
+                System.out.println("La valeur saisie n'est pas valide - tapez 1 ou 2.");
             }
 
-            // Il affiche à l'utilisateur le nombre de blocs en fonction du type à contenu par le Kit.
-            System.out.println("Voici le nombre de blocs de chaque type contenu dans le Kit de démarrage : ");
-            Map<Type, Integer> quantiteBloc = new TreeMap<Type, Integer>(); // La TreeMap permet de trier les entrées par ordre alphabétique de la clé.
-            for (IBloc bloc : kit.getBlocs()) {
-                Type type = Type.valueOf(bloc.getClass().getSimpleName().toUpperCase());
-                int quantite = quantiteBloc.getOrDefault(type, 0) + 1; // Quantite existante + 1.
-                quantiteBloc.put(type, quantite);
-            }
-            Set<Type> types = quantiteBloc.keySet();
-            for(Type type : types) {
-                System.out.println(type.toString() + " " + quantiteBloc.get(type));
-            }
         } catch (IllegalBlocException e) {
             System.out.println("Impossible de construire le Kit de démarrage.");
+        }catch (IOException e){
+            logger.error("Impossible de lire la saisie utilisateur.");
         }
 
         logger.info("Arret du programme Epicrafter's Journey.");
--- src/ej/kits/KitDemarrage.java
+++ src/ej/kits/KitDemarrage.java
@@ -2,11 +2,16 @@ package ej.kits;
 
 import ej.exceptions.IllegalBlocException;
 import ej.blocs.IBloc;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.io.*;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class KitDemarrage {
+
+    private Logger logger= LogManager.getLogger(KitDemarrage.class);
 
     private Set<IBloc> blocs = new LinkedHashSet<IBloc>();
     private Set<String> motsCles = new LinkedHashSet<String>();
@@ -34,5 +39,34 @@ public class KitDemarrage {
 
     public Set<String> getMotsCles() {
         return motsCles;
+    }
+
+    public  void sauvegarder(){
+        StringBuilder builder = new StringBuilder();
+        builder.append("Kit de démarrage\n");
+        for (String motCle : motsCles){
+            builder.append(motCle + " ");
+        }
+        try {
+            // Acceder au fichier
+            BufferedWriter writer = new BufferedWriter(new FileWriter("kit.txt"));
+            writer.write(builder.toString());
+            writer.close();
+        } catch (IOException e) {
+            logger.error("Impossible d'écrire dans le fichier");
+        }
+    }
+
+    public void charger() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("kit.txt"))) {
+            String line = null;
+            while((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            logger.error("Le fichier kit.txt n'existe pas.");
+        } catch (IOException e) {
+            logger.error("Impossible de lire le fichier kit.txt");
+        }
     }
 }
